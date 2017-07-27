fc539122984995fc7f992f882187e78f20cabe3a
Nick Foden <nickfoden@gmail.com>
2017-07-26 22:46:54 -0400

    Added delete to projects

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import uuid from 'uuid';
 import Projects from './Components/Projects';
 import AddProject from './Components/AddProject';
 import './App.css';
@@ -16,14 +17,17 @@ class App extends Component {
   componentWillMount(){
     this.setState({projects: [
               {
+          id: uuid.v4(),
           title: 'Business Website',
           category: 'Web Design'
         },
         {
+          id: uuid.v4(),
           title: 'Social App',
           category: 'Mobile Development'
         },
         {
+          id: uuid.v4(),
           title: 'Ecommerce Shopping Cart',
           category: 'Web Development'
         } 
@@ -31,11 +35,24 @@ class App extends Component {
 
   }
 
+  handleAddProject(project){
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects:projects});
+  }
+
+  handleDeleteProject(id){
+    let projects = this.state.projects;
+    let index = projects.findIndex(x => x.id === id);
+    projects.splice(index, 1);
+    this.setState({projects:projects});
+  }
+
   render() {
     return (
       <div className="App">
-         <AddProject />
-         <Projects projects={this.state.projects} />
+         <AddProject addProject={this.handleAddProject.bind(this)} />
+         <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)}/>
       </div>
     );
   }
